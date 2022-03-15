8126e65a05a436da6a010647f97f0f1de3f167b5
Kate2503 <ekaterinka25@gmail.com>
2022-03-15 22:01:47 +0300

    add Script sql

---
+++ Script-1.sql
@@ -0,0 +1,12 @@
+select * from students;
+select id from students;
+select name from students;
+select email from students;
+select name,email from students;
+select id,name,email,created_on  from students;
+select * from students where password='12333';
+select * from students where created_on = '2021-03-26 00:00:00';
+select * from students where name='Marlin';
+select * from students where name like '%8';
+select * from students where name like '%a%';
+
